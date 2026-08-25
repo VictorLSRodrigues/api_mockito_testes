03815dcd84207aa8698d3c24e426390454f9a756
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2026-08-25 19:21:32 -0300

    endpoint create para user

--- src/main/java/br/com/dicasdeumdev/api/resorces/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resorces/UserResource.java
@@ -5,10 +5,8 @@ import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,5 +32,13 @@ public class UserResource {
         return ResponseEntity.ok()
                 .body(service.findAll()
                         .stream().map(x ->mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj){
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri()
+        ).build();
     }
 }
