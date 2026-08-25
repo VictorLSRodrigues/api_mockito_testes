60079d3ed82d7ad8d2e3a90c040ff68f7c81e02d
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2026-08-25 17:29:01 -0300

    tratamento de execao para obj nao encontrado

---
+++ src/main/java/br/com/dicasdeumdev/api/services/exceptions/ObjectNotFoudException.java
@@ -0,0 +1,9 @@
+package br.com.dicasdeumdev.api.services.exceptions;
+
+public class ObjectNotFoudException extends RuntimeException{
+
+    public ObjectNotFoudException(String message) {
+        super(message);
+    }
+
+}
